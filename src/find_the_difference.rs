28a58df811b399a9c475a41bea77b00e8ded10f9
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-09-26 15:54:18 +0530

    Solve https://leetcode.com/problems/find-the-difference/ optimised

--- src/find_the_difference.rs
+++ src/find_the_difference.rs
@@ -4,15 +4,18 @@ struct Solution {}
 
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut letters = vec![0; 26];
-        let a_pos = 'a' as usize;
+        let mut letters = vec![0_i16; 26];
+        let a_pos = 'a' as u8;
 
-        s.chars().into_iter().for_each(|x| {
-            letters[x as usize - a_pos] += 1;
-        });
-        t.chars().into_iter().for_each(|x| {
-            letters[x as usize - a_pos] -= 1;
-        });
-        return (letters.iter().position(|x| (*x == 1 || *x == -1)).unwrap() + a_pos) as u8 as char;
+        s.chars()
+            .into_iter()
+            .zip(t.chars().into_iter())
+            .for_each(|(x, y)| {
+                letters[(x as u8 - a_pos) as usize] += 1;
+                letters[(x as u8 - a_pos) as usize] -= 1;
+            });
+        letters[(t.chars().into_iter().last().unwrap() as u8 - a_pos) as usize] -= 1;
+
+        return (letters.iter().position(|x| (*x == 1 || *x == -1)).unwrap() as u8 + a_pos) as char;
     }
 }
